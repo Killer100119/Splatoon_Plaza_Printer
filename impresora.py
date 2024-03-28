cfedb317eef4bc9e43ef8994c6c167efc13fd609
Killer1001 <46823576+Killer100119@users.noreply.github.com>
2024-03-28 06:01:27 +0100

    Translated some comments and functions to english

--- impresora.py
+++ impresora.py
@@ -1,9 +1,7 @@
 from conexion import *
 import os
 
-ruta_imagen = os.path.join("Imagenes", "demo7.png")
-
-def imprimir():
+def imprimir(ruta_imagen):
     with Image.open(ruta_imagen).convert("1") as im:
         pix = im.load()
         y = 0
@@ -12,24 +10,24 @@ def imprimir():
                 for x in reversed(range(320)):
                     if pix[x, y] < 125:
                         cerrar_rele_3()
-                        print("Dibuja")
+                        print("Draw")
                     cerrar_rele_1()
-                    print("Izquierda")
+                    print("Left")
             elif y % 2 == 0:
                 for x in (range(320)):
                     if pix[x,y] < 125:
                         cerrar_rele_3()
-                        print("Dibuja")
+                        print("Draw")
                     cerrar_rele_0()
-                    print("Derecha")
+                    print("Right")
             y += 1
-            print("Abajo")
+            print("Down")
             cerrar_rele_2()
         return
 
 #-------------------------------------------------------------------------
 
-def imprimir_optimizado():
+def imprimir_optimizado(ruta_imagen):
     with Image.open(ruta_imagen).convert("1") as im:
         imagen = im.load()
         posX = 0
@@ -41,60 +39,60 @@ def imprimir_optimizado():
             
             analisis = analizar_linea(imagen, posY)
             if len(analisis) == 0:
-                #Bajar linea si está vacia
+                #Skip line if empty
                 cerrar_rele_2()
                 posY = posY +1
-                print("Abajo")
+                print("Down")
 
 #-----------------------------------------------------------------------------
-#                      Fase de movimiento:
+#                      MOVE PHASE:
                 
             else:
-                #Nos movemos al pixel más cercano
-                objetivo = pixel_mas_cercano(posX, analisis[0], analisis[1])
-                #En caso de que haya que moverse a la izquierda
+                #Move to closest pixel
+                objetivo = closest_pixel(posX, analisis[0], analisis[1])
+                #Moving towards left
                 if posX > objetivo:
                     while posX != objetivo:
                         cerrar_rele_1()
                         posX = posX -1
-                        print("Izquierda")
-                #En caso de que haya que moverse a la derecha        
+                        print("Left")
+                #Moving towards right      
                 elif posX < objetivo:
                     while posX != objetivo:
                         cerrar_rele_0()
                         posX = posX +1
-                        print("Derecha")
+                        print("Right")
 
 #-----------------------------------------------------------------------------
-#                      Fase de dibujo:
+#                     DRAW PHASE:
 
-                #Tenemos que dibujar hacia la derecha
+                #Drawing towards right
                 if posX == analisis[0]:
                     while posX != analisis[1]:
                         if imagen[posX, posY] < 125:
                             cerrar_rele_3()
-                            print("Dibuja")
+                            print("Draw")
                         cerrar_rele_0()
                         posX = posX +1
-                        print("Derecha")
+                        print("Right")
                 
-                #Tenemos que dibujar hacia la izquierda
+                #Drawing towards left
                 elif posX == analisis[1]:
                     while posX != analisis[0]:
                         if imagen[posX, posY] < 125:
                             cerrar_rele_3()
-                            print("Dibuja")
+                            print("Draw")
                         cerrar_rele_1()
                         posX = posX -1
-                        print("Izquierda")
+                        print("Left")
 
 #-----------------------------------------------------------------------------
-#                   Fase de fin de linea:
+#                   END OF LINE:
 
-                #Fin de linea
+                #End of line
                 cerrar_rele_2()
                 posY = posY +1
-                print("Abajo")
+                print("Down")
                 
 
                 
@@ -106,7 +104,7 @@ def imprimir_optimizado():
 
 def analizar_linea(imagen, Y):
     '''
-    Devuelve las coordenadas del primer y ultimo pixel que hay que pintar en la linea dada
+    Returns x,y of first and last black pixel on the line given
     '''
     res = []
     for x in (range(320)):
@@ -116,7 +114,7 @@ def analizar_linea(imagen, Y):
         return ()
     return (res[0],res[-1])
         
-def pixel_mas_cercano(posX, pix_izquierdo, pix_derecho):
+def closest_pixel(posX, pix_izquierdo, pix_derecho):
   diferencia_1 = abs(posX - pix_izquierdo)
   diferencia_2 = abs(posX - pix_derecho)
 
@@ -130,25 +128,24 @@ def pixel_mas_cercano(posX, pix_izquierdo, pix_derecho):
 
 def ejecutar():
     inicio()
-    print("RECUERDA: Ctrl + c Para parar un programa manualmente")
-    res = input("PROGRAMA LISTO [1 -> Versión 1.0 // 2 -> Versión 2.0, cualquier otra cosa para cancelar]:")
+    print("REMEMBER: Ctrl + c to stop execution")
+    res = input("PROGRAM READY [1 -> Version 1.0 // 2 -> Version 2.0, any other key to cancel]:")
+    nombre_archivo = input("File name (File sufix must be included, for example: 'Demo.png')")
+    ruta_imagen = os.path.join("Imagenes", nombre_archivo)
     if res == "1":
         cerrar_rele_3()
         time.sleep(2)
         cerrar_rele_3()
         time.sleep(1)
-        imprimir()
+        imprimir(ruta_imagen)
     if res == "2":
         cerrar_rele_3()
         time.sleep(2)
         cerrar_rele_3()
         time.sleep(1)
-        imprimir_optimizado()
+        imprimir_optimizado(ruta_imagen)
     return
 
 if __name__ == "__main__": 
     ejecutar()
     #test_infinito()
-    '''with Image.open(ruta_imagen).convert("1") as im:
-        pix = im.load()
-        print(pix[10,10])'''
